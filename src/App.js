f4845f5286af260b96b088703b696365d29c9170
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2020-12-04 19:36:02 +0300

    Project structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
     return (
         <div className="App">
+            <Header />
+            <div className="wrapper">
+                <Navbar />
+                <main>
+
+                </main>
+            </div>
         </div>
     );
 }
