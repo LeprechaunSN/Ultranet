f881fb0ca4cde732b120cde44afdc2105d9fb24c
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2020-12-04 19:48:46 +0300

    Write header component and styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <header>
-            
+        <header className={s.header}>
+            <div className="wrapper">
+                <img className={s.logo} alt="Ultranet" src="https://demo.joomlashine.com/joomla-templates/jsn_ultranet/pro//templates/jsn_ultranet/images/logo.png" />
+            </div>
         </header>
     );
 }
