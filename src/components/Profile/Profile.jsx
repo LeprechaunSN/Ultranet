e87369844fc0370c52184f1334443d49717f86f9
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2021-02-16 23:08:46 +0300

    Add preloader, user data from props to profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
-import Posts from './Posts/Posts'
+import Preloader from '../common/Preloader/Preloader';
+import PostsContainer from './Posts/PostsContainer'
 import s from './Profile.module.css'
+import userPhoto from '../../assets/images/user-photo.png'
+import Status from './Status/Status';
 
 function Profile(props) {
+    if(!props.profile)
+        return <Preloader />;
+        
     return (
         <div>
             <div className={s.profileImage} >
-                <img src="https://upload.wikimedia.org/wikipedia/commons/8/80/140-P1020281_-_Flickr_-_Laurie_Nature_Bee.jpg" alt="img"/>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/8/80/140-P1020281_-_Flickr_-_Laurie_Nature_Bee.jpg" alt="img" />
             </div>
-            <div className={s.avaAndDescription}>
-                <img className={s.avatar} src="https://storage.theoryandpractice.ru/tnp/uploads/image_unit/000/156/586/image/base_87716f252d.jpg" alt="ava"/>
-                <div className={s.userName}>
-                    Sveta Nikonova
+            <div className={s.profileInfo}>
+                <img className={s.avatar} src={props.profile.photos.large || userPhoto} alt="ava" />
+                <div className={s.userInfo}>
+                    <div className={s.userName}>
+                        {props.profile.fullName}
+                    </div>
+                    <div>{props.profile.aboutMe}</div>
+                    <Status status={props.status} updateStatus={props.updateStatus} />
                 </div>
             </div>
-            <Posts posts={props.posts}  />
+            <div className={s.contacts}>
+
+            </div>
+            <PostsContainer />
         </div>
     );
 }
