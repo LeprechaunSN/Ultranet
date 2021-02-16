46d35d71694c968b27fc739a8631a7872ba6d73b
LeprechaunSN <sveta.nikonova.2014@mail.ru>
2021-02-16 23:06:27 +0300

    Add adding post form component to posts component

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import AddPostForm from './Post/AddPostForm/AddPostForm';
 import Post from './Post/Post'
 import s from './Posts.module.css'
 
 function Posts(props) {
+
+    const addPost = (values) => {
+        props.addPost(values.newPostText);
+    }
 
     let posts = props.posts.map( (post) => <Post post={post} />)
 
     return (
         <div className={s.posts}>
             <span className={s.postsTitle}>My posts</span>
-            <div className={s.addPost}>
-                <textarea rows="4" className={s.postContent}>Your news...</textarea>
-                <button className={s.addPostButton}>Send</button>
-            </div>
+            <AddPostForm onSubmit={addPost} />
             <div>
                 {posts}
             </div>
